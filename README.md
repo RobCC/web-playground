b6b4b151589f44d898c76445013525b7f4260c8b
robcc <rrc0138@gmail.com>
2019-10-30 16:10:26 -0600

    Add production env to build script

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ A react playground.
 Webpack config vs .babelrc
 
 https://stackoverflow.com/questions/48476373/whats-the-difference-when-configuring-webpack-babel-loader-vs-configuring-it-wit/48485578#48485578
+
+https://webpack.js.org/guides/environment-variables/
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,41 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          { loader: 'css-loader' },
-          { loader: 'postcss-loader' },
-          {
-            loader: 'sass-loader',
-            options: {
-              implementation: require('sass')
-            }
+module.exports = env => {
+  console.log(`Environment: ${env.NODE_ENV}`);
+
+  return {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader"
           }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({ filename: 'bundle.css' })
-  ]
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            { loader: MiniCssExtractPlugin.loader },
+            { loader: "css-loader" },
+            { loader: "postcss-loader" },
+            {
+              loader: "sass-loader",
+              options: {
+                implementation: require("sass")
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [new MiniCssExtractPlugin({ filename: "bundle.css" })]
+  };
 };
