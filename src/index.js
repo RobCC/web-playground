80ccff779bf569b00f9ae48502d3110acf5d9abd
robcc <rrc0138@gmail.com>
2019-11-04 09:41:40 -0600

    Insert App component into body

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import ReactDOM from 'react-dom';
+import { App } from './components/App';
 import styles from './index.scss';
 
-console.log('Hello world');
+const insertRoot = () => {
+  const root = document.createElement('div');
+
+  root.setAttribute('id', 'root');
+  document.body.appendChild(root);
+
+  return root;
+};
+
+ReactDOM.render(App, insertRoot());
