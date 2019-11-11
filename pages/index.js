d339b51c9ebae563f2864e6ae7cfb761ef60e61c
robcc <rrc0138@gmail.com>
2019-11-11 15:11:30 -0600

    Add router/link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
+import Router from 'next/router';
 
 const indexPage = () => (
-  <div><h1>The Main Page</h1></div>
+  <div>
+    <h1>The Main Page</h1>
+    <p>
+      Go to
+      <Link href="/auth">
+        <a>Auth</a>
+      </Link>
+
+      <button type="button" onClick={() => Router.push('/auth')}>To Auth</button>
+    </p>
+  </div>
 );
 
 export default indexPage;
