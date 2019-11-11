ce05898fb4dea20622e426a0563acf38fb1682b9
robcc <rrc0138@gmail.com>
2019-11-11 15:35:19 -0600

    Add error page. Remove webpack

--- components/User.js
+++ components/User.js
@@ -2,9 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const User = ({ name, age }) => (
-  <div>
+  <div className="user">
     <h1>{name}</h1>
     <p>Age: {age}</p>
+    <style jsx>{`
+      div.user {
+        border: 1px solid #eee;
+        box-shadow: 0 2px 3px #ccc;
+        font-family: sans-serif;
+      }
+    `}
+    </style>
   </div>
 );
 
---
+++ pages/_error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+const errorPage = () => (
+  <div>
+    <h1>Oops, something went wrong</h1>
+    <h2>
+      Try going
+      <Link><a>Home</a></Link>
+    </h2>
+  </div>
+);
+
+export default errorPage;
