5c2015bf0e61f15036f1a06b1eb8f14486e61bb5
robcc <rrc0138@gmail.com>
2019-12-04 09:24:28 -0600

    Add Material examples

--- src/views/Material/Material.js
+++ src/views/Material/Material.js
@@ -1,8 +1,84 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import {
+  Button,
+  Checkbox,
+  Container,
+} from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import {
+  createMuiTheme,
+  makeStyles,
+  ThemeProvider,
+} from '@material-ui/core/styles';
+import { orange, pink } from '@material-ui/core/colors';
 
-const Material = () => (
-  <Button variant="contained" color="primary">Hello World</Button>
-);
+
+// Styles for checkbox (specific styling)
+const useStyles = makeStyles((theme) => ({
+  root: {
+    color: theme.status.danger,
+    '&$checked': {
+      color: theme.status.danger,
+    },
+  },
+  checked: {},
+}));
+
+
+// Custom checkbox
+const MyCheckbox = () => {
+  const { root, checked } = useStyles();
+
+  return <Checkbox classes={{ root, checked }} />;
+};
+
+// Main theme (changes defaults)
+const theme = createMuiTheme({
+  status: {
+    danger: orange[500],
+  },
+  palette: {
+    primary: pink,
+  },
+});
+
+// Button styles (specific styling)
+const useBtnStyles = makeStyles(() => ({
+  label: {
+    color: '#000000',
+  },
+}));
+
+const Material = () => {
+  const { label } = useBtnStyles();
+
+  return (
+    <div>
+      <Checkbox />
+      <ThemeProvider theme={theme}>
+        <Button variant="contained" color="primary" classes={{ label }}>
+          Hello World
+        </Button>
+        <MyCheckbox />
+      </ThemeProvider>
+
+      <Box color="white" bgcolor="#333" p={2} m={1} borderRadius={15}>JSS</Box>
+      <Container maxWidth="sm">
+        <Box
+          p={2}
+          m={1}
+          display="flex"
+          color="black"
+          bgcolor="#bbb"
+          borderRadius={15}
+          alignItems="center"
+          justifyContent="center"
+        >
+          JSS
+        </Box>
+      </Container>
+    </div>
+  );
+};
 
 export default Material;
